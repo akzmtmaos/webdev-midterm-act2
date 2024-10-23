805db5fa80db26211d606f9c49bb8f5e3abfed4e
akzmtmaos <134627851+akzmtmaos@users.noreply.github.com>
2024-10-23 19:39:49 +0800

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,10 @@
+/*
+CREATE DATABASE school;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+*/
